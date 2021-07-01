dad47b947ff822d700be43d75e72ed75bc6bc23f
Jay Zhang <>
2021-07-01 14:20:16 +0800

    feat: also print app build type

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 
 #include "hello.h"
+#include <iostream>
 
 int main(int argc, char** argv) {
+    
+    #ifdef NDEBUG
+    std::cout << "hello app Release!" <<std::endl;
+    #else
+    std::cout << "hello app Debug!" <<std::endl;
+    #endif
+
     hello();
+
     return 0;
 }
